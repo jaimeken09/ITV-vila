3a0736bcaa2a7611a80f38f1e3d39962099a1a5f
jaime <jaime.ayuala.1@educa.jcyl.es>
2022-12-15 19:22:07 +0100

    se crea comun con cosas comunes de vehiculos;intento hace todo, pero no me he eho amigo de la app funcionando.

---
+++ src/ed/data/camion.java
@@ -0,0 +1,18 @@
+package ed.data;
+
+public class camion {
+    public String codiggo vehiculo;
+    public String plate number;
+    public String marca;
+    public String model;
+    public String color;
+    public String Tara;
+    public String longitud;
+    public String year matricula;
+
+    public void setCodiggo(String codiggo) {
+        this.codiggo = codiggo;
+        public void getCodigo(String codigo );
+
+    }
+}
